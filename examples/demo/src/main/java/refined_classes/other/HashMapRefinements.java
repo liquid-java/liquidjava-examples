11c32132af686e6f406dee4e1967e2df4a568eb3
Márcio Caetano <rodrigomilisse@gmail.com>
2025-11-19 11:24:11 +0000

    HashMap fix

--- examples/demo/src/main/java/refined_classes/other/HashMapRefinements.java
+++ examples/demo/src/main/java/refined_classes/other/HashMapRefinements.java
@@ -1,4 +1,4 @@
-package refined_classes;
+package refined_classes.other;
 
 import liquidjava.specification.*;
 
@@ -12,33 +12,23 @@ import java.util.function.Function;
 @StateSet({ "empty", "non-empty" })
 @RefinementAlias("Empty(HashMap h) { size(h) == 0 }")
 @RefinementAlias("NotEmpty(HashMap h) { size(h) > 0 }")
-@RefinementAlias("Insert(HashMap h) { size(h) == size(old(h)) + 1 && (size(h) == capacity(h) * loadFactor(h) --> (capacity(h) == capacity(old(h)) * 2)) }") // TODO: remove because its aproximatelly double and probably useless
+@RefinementAlias("Insert(HashMap h) { size(h) == size(old(h)) + 1 }")
 @RefinementAlias("Remove(HashMap h) { size(h) == size(old(h)) - 1 }")
-@Ghost("int capacity")
 @Ghost("int size")
-@Ghost("float loadFactor")
 public abstract class HashMapRefinements<K, V> {
 
 	// ======== Constructors ======== //
 
 	@StateRefinement(to = "Empty(this)")
-	@StateRefinement(to = "capacity(this) == initialCapacity")
-	@StateRefinement(to = "loadFactor(this) == loadFactor")
 	public abstract void HashMap(int initialCapacity, float loadFactor);
 
 	@StateRefinement(to = "Empty(this)")
-	@StateRefinement(to = "capacity(this) == initialCapacity")
-	@StateRefinement(to = "loadFactor(this) == 0.75f")
 	public abstract void HashMap(int initialCapacity);
 
 	@StateRefinement(to = "Empty(this)")
-	@StateRefinement(to = "capacity(this) == 16")
-	@StateRefinement(to = "loadFactor(this) == 0.75f")
 	public abstract void HashMap();
 
 	@StateRefinement(to = "Empty(this) || NotEmpty(this)")
-	@StateRefinement(to = "capacity(this) == 0 || capacity(this) > 0") // Size of m is inaccessible
-	@StateRefinement(to = "loadFactor(this) == 0.75f")
 	public abstract void HashMap(Map<? extends K, ? extends V> m);
 
 	// ======== Observers ========= //
@@ -65,7 +55,6 @@ public abstract class HashMapRefinements<K, V> {
 	public abstract V put(K key, V value);
 
 	@StateRefinement(to = "size(this) >= size(old(this))")
-	@StateRefinement(to = "capacity(this) >= capacity(old(this))")
 	public abstract void putAll(@Refinement("m != null") Map<? extends K, ? extends V> m);
 
 	@StateRefinement(to = "Remove(this)")
@@ -74,16 +63,16 @@ public abstract class HashMapRefinements<K, V> {
 	@StateRefinement(to = "Empty(this)")
 	public abstract void clear();
 
-	@StateRefinement(to = "Insert(this) || size(this) = size(old(this))")
+	@StateRefinement(to = "Insert(this) || size(this) == size(old(this))")
 	public abstract V computeIfAbsent(K key, @Refinement("mappingFunction != null") Function<? super K,? extends V> mappingFunction);
 
-	@StateRefinement(to = "Remove(this) || size(this) = size(old(this))")
+	@StateRefinement(to = "Remove(this) || size(this) == size(old(this))")
 	public abstract V computeIfPresent(K key, @Refinement("remappingFunction != null") Function<? super K,? extends V> remappingFunction);
 
-	@StateRefinement(to = "Insert(this) || Remove(this) || size(this) = size(old(this))")
+	@StateRefinement(to = "Insert(this) || Remove(this) || size(this) == size(old(this))")
 	public abstract V compute(K key, @Refinement("remappingFunction != null") BiFunction<? super K,? super V,? extends V> remappingFunction);
 
-	@StateRefinement(to = "Insert(this) || Remove(this) || size(this) = size(old(this))")
+	@StateRefinement(to = "Insert(this) || Remove(this) || size(this) == size(old(this))")
 	public abstract V merge(K key, V value, @Refinement("remappingFunction != null") BiFunction<? super V,? super V,? extends V> remappingFunction);
 
 	/* public static newHashMap(int numMappings) Not included because its static */
